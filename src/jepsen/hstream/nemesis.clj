a6433dd6099f49b12eef082f04c2cbc19947b926
Commelina <maosics@gmail.com>
2022-03-11 14:10:31 +0800

    nodes: take zk and ld into consideration

--- src/jepsen/hstream/nemesis.clj
+++ src/jepsen/hstream/nemesis.clj
@@ -1,24 +1,13 @@
 (ns jepsen.hstream.nemesis
   (:gen-class)
-  (:require [clojure.string :as str]
-            [clojure.core.reducers :as reducers]
-            [clojure.tools.logging :refer :all]
-            [clojure.java.shell :refer [sh]]
-            [clojure.pprint :as pp]
+  (:require [clojure.tools.logging :refer :all]
             [jepsen [db :as db] [cli :as cli] [checker :as checker]
              [client :as client] [control :as c] [generator :as gen]
              [independent :as independent] [nemesis :as nemesis]
-             [tests :as tests] [util :refer [timeout]]]
-            [jepsen.os.ubuntu :as ubuntu]
-            [jepsen.checker.timeline :as timeline]
-            [knossos.model :as model]
-            [random-string.core :as rs]
-            [jepsen.control.docker :as docker]
-            [jepsen.control.core :as cc]
+             [tests :as tests]]
             [jepsen.hstream.client :refer :all]
-            [jepsen.hstream.utils :refer :all]
             [jepsen.hstream.mvar :refer :all]
-            [clojure.stacktrace :refer [e]]))
+            [jepsen.hstream.utils :refer :all]))
 
 (defn kill-node
   [node]
@@ -29,13 +18,13 @@
                 "-9" "hstream-server"
                 "||" "true")))
 
-(defn is-node-dead
+(defn is-hserver-on-node-dead?
   [node]
   (let [shell-out (c/on node
                         (c/exec "pgrep" "-x" "hstream-server" "||" "true"))]
     (empty? shell-out)))
 
-(defn is-node-alive
+(defn is-hserver-on-node-alive?
   [node]
   (let [shell-out (c/on node
                         (c/exec "pgrep" "-x" "hstream-server" "||" "true"))]
@@ -43,18 +32,24 @@
 
 (defn restart-node [node] (c/on node (c/exec "/bin/start-hstream-server")))
 
-(defn find-alive-nodes [test] (into [] (filter is-node-alive (:nodes test))))
+(defn find-hserver-alive-nodes
+  [test]
+  (into []
+        (filter is-hserver-on-node-alive? (remove #{"zk" "ld"} (:nodes test)))))
 
-(defn find-dead-nodes [test] (into [] (filter is-node-dead (:nodes test))))
+(defn find-hserver-dead-nodes
+  [test]
+  (into []
+        (filter is-hserver-on-node-dead? (remove #{"zk" "ld"} (:nodes test)))))
 
 (defn nemesis+
   []
   (reify
     nemesis/Nemesis
-      (nemesis/setup! [this test] this)
-      (nemesis/invoke! [this test op]
+      (nemesis/setup! [this _] this)
+      (nemesis/invoke! [_ test op]
         (case (:f op)
-          :start (let [alive-nodes (find-alive-nodes test)]
+          :start (let [alive-nodes (find-hserver-alive-nodes test)]
                    (if (<= (count alive-nodes) 1)
                      (assoc op :value "killing skipped")
                      (let [node (rand-nth alive-nodes)]
@@ -62,7 +57,7 @@
                        (assoc op
                          :value "killed"
                          :node node))))
-          :stop (let [dead-nodes (find-dead-nodes test)]
+          :stop (let [dead-nodes (find-hserver-dead-nodes test)]
                   (if (empty? dead-nodes)
                     (assoc op :value "restarting skipped")
                     (let [node (rand-nth dead-nodes)]
@@ -70,4 +65,6 @@
                       (assoc op
                         :value "restarted"
                         :node node))))))
-      (nemesis/teardown! [this test])))
+      (nemesis/teardown! [_ _])))
+
+(defn zk-nemesis [] (nemesis/partition-random-node))
