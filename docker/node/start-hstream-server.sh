f188710aa703e4188c02e7a355ff2c1b9cc4018f
Commelina <maosics@gmail.com>
2022-03-15 14:11:24 +0800

    docker: store all logs in tmp file

--- docker/node/start-hstream-server.sh
+++ docker/node/start-hstream-server.sh
@@ -2,14 +2,14 @@
 
 SERVER_ID=$(shuf -i 1-4294967296 -n 1)
 MY_IP=$(hostname -I | head -n1 | awk '{print $1;}')
-nohup hstream-server \
-      --config-path /etc/hstream/config.yaml \
-      --host 0.0.0.0 \
-      --port 6570    \
-      --address $MY_IP \
-      --store-config /root/logdevice.conf \
-      --zkuri "zookeeper:2188" \
-      --server-id $SERVER_ID \
-      --log-level debug \
-      --log-with-color \
-      >/dev/null 2>&1 & disown
+hstream-server \
+    --config-path /etc/hstream/config.yaml \
+    --host 0.0.0.0 \
+    --port 6570    \
+    --address $MY_IP \
+    --store-config /root/logdevice.conf \
+    --zkuri "zookeeper:2188" \
+    --server-id $SERVER_ID \
+    --log-level debug \
+    --log-with-color \
+    >/tmp/node.log 2>&1 &
