7d90705e9c35542871ce852e461619e8f45ee74f
Commelina <maosics@gmail.com>
2024-07-15 05:55:30 +0300

    ci: hornbill

--- scripts/build_hornbill_base.sh
+++ scripts/build_hornbill_base.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 docker build -t jepsen-hornbill:base \
        --build-arg "USE_CHINA_MIRROR=false" \
-       --build-arg "HORNBILL_IMAGE=ghcr.io/hstreamdb/hornbill:v1.0.0-m0" \
+       --build-arg "HORNBILL_IMAGE=ghcr.io/hstreamdb/hornbill:latest" \
        ./docker/base-hornbill
